7308b5c2bb4a07735900b43b4d860270078c9d49
Ivana D <ivana@bitlab.rs>
2021-10-29 21:17:26 +0200

    Rutiranje. view i organizacija blade datoteka, migracije

---
+++ resources/views/posts/posts.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.user-layout')
+@section('content')
+Ovde cemo prikazati sve postojece postove i omoguciti useru da izvrsi CRUD operacije
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('post', function () {
+    return view('post');
+});
+Route::get('dashboard', function () {
+    return view('posts.posts');
 });
